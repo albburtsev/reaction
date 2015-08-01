a9b9d629c6771fedbb5924a80af7ead8475b82d3
Alexander Burtsev <albburtsev@gmail.com>
2015-08-01 13:38:04 +0300

    System notifications about errors and warnings

--- server.js
+++ server.js
@@ -1,12 +1,57 @@
-var webpack = require('webpack'),
-    WebpackDevServer = require('webpack-dev-server'),
-    config = require('./webpack.config');
+var chalk = require('chalk'),
+    webpack = require('webpack'),
+    notifier = require('node-notifier'),
+    DevServer = require('webpack-dev-server'),
+    config = require('./webpack.config'),
+    builder = webpack(config);
 
-new WebpackDevServer(webpack(config), {
+builder.plugin('done', function (stats) {
+    var icon,
+        title,
+        message,
+        resource;
+
+    if (stats.hasErrors()) {
+        var error = stats.compilation.errors[0];
+
+        title = 'Error';
+        icon = '\uD83D\uDEA8';
+        message = error.error.toString();
+        resource = error.module.resource;
+    } else if (stats.hasWarnings()) {
+        var warning = stats.compilation.warnings[0];
+
+        title = 'Warning';
+        icon = '\uD83D\uDEA8';
+        message = warning.warning.toString();
+        resource = warning.module.resource;
+    }
+
+    if (message) {
+        message = chalk.stripColor(message);
+        message = message.trim().split('\n');
+
+        var fileName = resource.replace(/^.*\//g, ''),
+            errorMessage = message[1].replace(/ +/g, ' '),
+            errorStat = message.pop();
+
+        notifier.notify({
+            title: icon + ' ' + title + ': ' + fileName,
+            subtitle: errorMessage,
+            message: errorStat,
+            open: 'file://' + resource
+        });
+    }
+});
+
+var server = new DevServer(builder, {
     hot: true,
+    noInfo: true,
     publicPath: config.output.publicPath,
     historyApiFallback: true
-}).listen(3000, 'localhost', function(err, result) {
+});
+
+server.listen(3000, 'localhost', function(err, result) {
     if (err) {
         console.log(err);
     }
