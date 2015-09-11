cbb85d858284311670d7acff919b2cdb817ebe05
Alexander Burtsev <a.burtsev@zmeke.com>
2015-09-11 17:12:56 +0300

    Update dependencies: react-router v1.0.0-beta4

--- src/app.js
+++ src/app.js
@@ -1,20 +1,18 @@
 import './app.styl';
 
-import {ROUTE_ENTRY} from 'routes';
-import {Router, Route, Redirect} from 'react-router';
-import {history} from 'react-router/lib/BrowserHistory';
+import {createHistory} from 'history';
+import {Router, Route, IndexRoute} from 'react-router';
 
 import Root from 'components/Root/Root.jsx';
 import PageEntry from 'components/PageEntry/PageEntry.jsx';
 
-let rootElement = document.querySelector('.app');
+let rootElement = document.querySelector('.app'),
+    history = createHistory();
 
 ReactDOM.render((
     <Router history={history}>
-        {/* @todo: waiting for IndexRoute: https://github.com/rackt/react-router/issues/1730#issuecomment-138986032 */}
-        <Redirect from="/" to={ROUTE_ENTRY} />
         <Route path="/" component={Root}>
-            <Route path={ROUTE_ENTRY} component={PageEntry} />
+            <IndexRoute component={PageEntry} />
         </Route>
     </Router>
 ), rootElement);
--- src/routes.js
+++
@@ -1,9 +0,0 @@
-/**
- * All available route's names
- * @todo: needs ES6-symbols instead of strings
- */
-export default {
-    ROUTE_ENTRY: 'entry',
-    ROUTE_SIGNIN: 'signin',
-    ROUTE_SIGNUP: 'signup'
-};
