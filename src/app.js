f88fa8d141a812132dc5aea60597b86cddedbbdf
Alexander Burtsev <albburtsev@gmail.com>
2016-02-26 22:36:14 +0300

    Supported base path for applications placed not in root location

--- src/app.js
+++ src/app.js
@@ -9,11 +9,12 @@ import Root from 'components/Root/Root.jsx';
 import PageEntry from 'components/PageEntry/PageEntry.jsx';
 
 let rootElement = document.querySelector('.app'),
+    basePath = global.basePath || '/',
     history = createHistory();
 
 ReactDOM.render((
     <Router history={history}>
-        <Route path="/" component={Root}>
+        <Route path={basePath} component={Root}>
             <IndexRoute component={PageEntry} />
         </Route>
     </Router>
