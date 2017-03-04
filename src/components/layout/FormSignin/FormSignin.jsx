7fbb5eb2437bcbb7752bb9cae8aafc857041bd3e
Alexander Burtsev <albburtsev@gmail.com>
2017-03-04 13:42:30 +0300

    Add `Input` component

--- src/components/layout/FormSignin/FormSignin.jsx
+++ src/components/layout/FormSignin/FormSignin.jsx
@@ -5,9 +5,12 @@ import {PATH_SIGNUP} from 'paths';
 
 import {Link} from 'react-router';
 import Button from 'components/common/Button/Button';
+import Input, {INPUT_TYPE_EMAIL, INPUT_TYPE_PASSWORD} from 'components/common/Input';
 
 const FormSignin = () =>
     <form>
+        <Input type={INPUT_TYPE_EMAIL} name="login" required autoFocus />
+        <Input type={INPUT_TYPE_PASSWORD} name="password" required />
         <p>
             Don't have an account?
             <Link to={PATH_SIGNUP}>Sign up</Link>
