df316efc66e69caf1118230846ccfe6aafcc825d
Alexander Burtsev <a.burtsev@zmeke.com>
2017-03-27 19:36:39 +0300

    Unit tests for `forms` store

--- src/store.js
+++ src/store.js
@@ -1,8 +1,11 @@
 import reducers from 'reducers';
 import thunkMiddleware from 'redux-thunk';
-import {createStore, applyMiddleware} from 'redux';
+import {createStore as createReduxStore, applyMiddleware} from 'redux';
 import {composeWithDevTools} from 'redux-devtools-extension/developmentOnly';
 
-export default createStore(reducers, composeWithDevTools(
-    applyMiddleware(thunkMiddleware)
-));
+export const createStore = () =>
+    createReduxStore(reducers, composeWithDevTools(
+        applyMiddleware(thunkMiddleware)
+    ));
+
+export default createStore();
