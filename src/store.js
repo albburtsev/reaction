23029ddba26a1fe930512bd9a246cc0575e14b77
Alexander Burtsev <a.burtsev@zmeke.com>
2017-03-27 18:18:34 +0300

    Added `redux-devtools-extension` for comfy debug

--- src/store.js
+++ src/store.js
@@ -1,4 +1,8 @@
-import {createStore} from 'redux';
 import reducers from 'reducers';
+import thunkMiddleware from 'redux-thunk';
+import {createStore, applyMiddleware} from 'redux';
+import {composeWithDevTools} from 'redux-devtools-extension/developmentOnly';
 
-export default createStore(reducers);
+export default createStore(reducers, composeWithDevTools(
+    applyMiddleware(thunkMiddleware)
+));
