c23f6b2d6d69a377f390489724444a927e53ee82
Alexander Burtsev <albburtsev@gmail.com>
2017-04-23 12:52:44 +0300

    Move `redux-devtools-extension` to dev dependencies

--- src/store.js
+++ src/store.js
@@ -4,8 +4,11 @@ import {createStore as createReduxStore, applyMiddleware} from 'redux';
 import {composeWithDevTools} from 'redux-devtools-extension/developmentOnly';
 
 export const createStore = () =>
-    createReduxStore(reducers, composeWithDevTools(
-        applyMiddleware(thunkMiddleware)
-    ));
+    createReduxStore(
+        reducers,
+        composeWithDevTools(
+            applyMiddleware(thunkMiddleware)
+        )
+    );
 
 export default createStore();
