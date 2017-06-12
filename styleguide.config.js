093f60808e2e4834bc4ac40dfdad8116e9401169
Alexander Burtsev <albburtsev@gmail.com>
2017-06-12 18:53:19 +0300

    The first step is troublesome

---
+++ styleguide.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+    verbose: true,
+    defaultExample: true,
+    title: 'React Style Guide Example',
+    // components: 'src/components/**/*.{jsx,tsx}',
+    components: 'src/components/common/Input/Input*.tsx',
+    propsParser: require('react-docgen-typescript').parse,
+    webpackConfig: {
+        module: {
+            loaders: [
+                {
+                    test: /\.(ts|tsx)?$/,
+                    loader: 'awesome-typescript-loader'
+                },
+                {
+                    test: /\.(js|jsx)$/,
+                    exclude: /node_modules/,
+                    loader: 'babel'
+                }
+            ]
+        }
+    }
+};
