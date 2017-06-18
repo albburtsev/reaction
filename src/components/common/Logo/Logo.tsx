7c5fe593bb51c15ce83fcb8e549741e5343fc0b5
Alexander Burtsev <albburtsev@gmail.com>
2017-06-18 21:50:13 +0300

    Try SFC Logo component with Styleguidist

--- src/components/common/Logo/Logo.tsx
+++ src/components/common/Logo/Logo.tsx
@@ -1,17 +1,10 @@
 import * as React from 'react';
 import * as styles from './Logo.styl';
 
-export interface LogoProps {};
-
 /**
  * Primitive logo with react icon
  */
-export class Logo extends React.Component<LogoProps, any> {
-    render() {
-        return (
-            <span className={styles.logo} />
-        );
-    }
-}
+export const Logo = () =>
+    <span className={styles.logo} />;
 
 export default Logo;
